e0f46d7d193991e81e80497503d060d8b83f0ffd
frank <frank.haugen@gmail.com>
2018-11-12 13:48:17 +0100

    Restructuring of repo

---
+++ WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dot_Net_Rocks_Player_WPF
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
