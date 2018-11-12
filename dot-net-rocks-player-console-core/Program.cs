4fadb5e85b41055e517fc4e063df9ac2b0bcb2e5
frank <frank.haugen@gmail.com>
2018-11-12 04:25:42 +0100

    created project

---
+++ dot-net-rocks-player-console-core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dot_net_rocks_player_console_core
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.ReadLine();
+		}
+	}
+}
